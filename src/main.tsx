aea47b83fed18cc55c762bba9e9c6938e308c0a1
h4x0rlol <mazafaker.com@gmail.com>
2022-10-29 19:28:04 +0600

    feat: add redux and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+	<React.StrictMode>
+		<Suspense fallback={<div>Loading...</div>}>
+			<App />
+		</Suspense>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
